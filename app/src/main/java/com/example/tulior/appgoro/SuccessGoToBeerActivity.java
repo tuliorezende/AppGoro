3869d2f2c642fc4c5a83009a3b49143842fe1525
tuliorezende <tulio.rezende@outlook.com>
2018-05-15 18:11:31 -0300

    Create success page

---
+++ app/src/main/java/com/example/tulior/appgoro/SuccessGoToBeerActivity.java
@@ -0,0 +1,13 @@
+package com.example.tulior.appgoro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuccessGoToBeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_go_to_beer);
+    }
+}
